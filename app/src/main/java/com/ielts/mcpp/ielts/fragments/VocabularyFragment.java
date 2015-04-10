1ec32afa398f65a2e944e45b13061a4353331e7a
boujas <mihalikvlad@gmail.com>
2015-04-10 17:57:19 +0300

    vocabulary list with search

--- app/src/main/java/com/ielts/mcpp/ielts/fragments/VocabularyFragment.java
+++ app/src/main/java/com/ielts/mcpp/ielts/fragments/VocabularyFragment.java
@@ -1,17 +1,30 @@
 package com.ielts.mcpp.ielts.fragments;
 
-import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.ielts.mcpp.ielts.R;
 
 
 public class VocabularyFragment extends Fragment {
+
+    private ListView listView;
+    ArrayAdapter<String> adapter;
+    EditText inputSearch;
+    private RecyclerView.LayoutManager mLayoutManager;
+    String products[] = {"relationship", "outsource", "congress", "problem", "stand", "code",
+            "restrict", "highbrow", "formula", "community", "fashion", "action", "statement"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,7 +34,40 @@ public class VocabularyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vocabulary, container, false);
+        View v = inflater.inflate(R.layout.fragment_vocabulary, null);
+        listView = (ListView) v.findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, products);
+        listView.setAdapter(adapter);
+        inputSearch = (EditText) v.findViewById(R.id.inputSearch);
+        inputSearch.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+                adapter.getFilter().filter(cs);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                          int arg3) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View view, int pos, long arg3) {
+                String item = (String) adapter.getItem(pos);
+                Toast.makeText(getActivity(), item + " выбран", Toast.LENGTH_LONG).show();
+            }
+        });
+        return v;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 }
